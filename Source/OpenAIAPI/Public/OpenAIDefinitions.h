318ed1969b3bafe904cc32ab70d3cac4d5ccae47
getnamo <getnamo@gmail.com>
2024-05-23 12:31:16 -0700

    add custom model
    - doesn't do anything atm, but used for separation

--- Source/OpenAIAPI/Public/OpenAIDefinitions.h
+++ Source/OpenAIAPI/Public/OpenAIDefinitions.h
@@ -29,6 +29,7 @@ enum class EOAChatEngineType : uint8
 	GPT_4 = 1 UMETA(ToolTip = "More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat. Will be updated with our latest model iteration."),
 	GPT_4_32k = 2 UMETA(ToolTip = "Same capabilities as the base gpt-4 model but with 4x the context length. Will be updated with our latest model iteration."),
 	GPT_4_TURBO = 3 UMETA(ToolTip = "The latest GPT-4 model with improved instruction following, JSON mode, reproducible outputs, parallel function calling, and more. Returns a maximum of 4,096 output tokens. This preview model is not yet suited for production traffic. Learn more."),
+	CUSTOM = 254 UMETA(ToolTip = "Non-standard model, pass model info by string."),
 };
 
 UENUM(BlueprintType)
@@ -123,7 +124,6 @@ struct FCompletion
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
 	int32 index = 0;
-	
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
 	FString finishReason = "";
